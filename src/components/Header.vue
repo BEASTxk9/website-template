f621946bb8c6fd9333b80f08f5953942942d7f1d
Stevens <shanes@belpark.sun.ac.za>
2023-04-04 11:55:59 +0200

    update

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,10 +11,12 @@
                             <h1>Shane Stevens</h1>
                             <h2>I am an Aspiring Fullstack Web Developer & Designer!</h2>
                             <h3>DEDICATED | DISCIPLINED | DIFFERENT</h3>
-
-                            <button>
-                                GLITCH
+                         
+                            <button class="mouse" scr="#about">
+                                <div class="scroll">
+                                </div>
                               </button>
+
                         </div>
      
 
@@ -56,11 +58,11 @@ header{
     font-size: 10rem;
 }
 #content h2{
-    font-size: 2rem;
+    font-size: 2.3rem;
 }
 #content h3{
     padding: 10px;
-    font-size: 1rem;
+    font-size: 1.3rem;
 }
 
 
@@ -68,110 +70,44 @@ header{
 
 
 /* scroll down button */
-button{
-border: 3px solid transparent;
-}
-
-button,button::after {
-    padding: 10px 50px;
-    font-size: 20px;
-    border: none;
-    border-radius: 5px;
-    color: white;
+.mouse {
+    width: 25px;
+    height: 40px;
+    border: 1px solid rgb(196, 196, 196);
     background-color: transparent;
+    border-radius: 13px;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
     position: relative;
+    left: 50%;
+    right: 50%;
+    margin-top: 10px;
   }
   
-  button::after {
-    --move1: inset(50% 50% 50% 50%);
-    --move2: inset(31% 0 40% 0);
-    --move3: inset(39% 0 15% 0);
-    --move4: inset(45% 0 40% 0);
-    --move5: inset(45% 0 6% 0);
-    --move6: inset(14% 0 61% 0);
-    clip-path: var(--move1);
-    content: 'GLITCH';
+  .scroll {
+    width: 3px;
+    height: 10px;
+    background-color: rgb(196, 196, 196);
+    border-radius: 2px;
     position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    display: block;
+    bottom: 9px;
+    animation: move_918 1.5s linear infinite;
   }
   
-  button:hover::after {
-    animation: glitch_4011 1s;
-    text-shadow: 10 10px 10px black;
-    animation-timing-function: steps(2, end);
-    text-shadow: -3px -3px 0px #1df2f0, 3px 3px 0px #E94BE8;
-    background-color: transparent;
-    border: 3px solid rgb(0, 255, 213);
-  }
-  
-  button:hover {
-    text-shadow: -1px -1px 0px #1df2f0, 1px 1px 0px #E94BE8;
-  }
-  
-  button:hover {
-    background-color: transparent;
-    border: 1px solid rgb(0, 255, 213);
-    box-shadow: 0px 10px 10px -10px rgb(0, 255, 213);
-  }
-  
-  @keyframes glitch_4011 {
+  @keyframes move_918 {
     0% {
-      clip-path: var(--move1);
-      transform: translate(0px,-10px);
-    }
-  
-    10% {
-      clip-path: var(--move2);
-      transform: translate(-10px,10px);
-    }
-  
-    20% {
-      clip-path: var(--move3);
-      transform: translate(10px,0px);
-    }
-  
-    30% {
-      clip-path: var(--move4);
-      transform: translate(-10px,10px);
-    }
-  
-    40% {
-      clip-path: var(--move5);
-      transform: translate(10px,-10px);
+      bottom: 9px;
     }
   
     50% {
-      clip-path: var(--move6);
-      transform: translate(-10px,10px);
-    }
-  
-    60% {
-      clip-path: var(--move1);
-      transform: translate(10px,-10px);
-    }
-  
-    70% {
-      clip-path: var(--move3);
-      transform: translate(-10px,10px);
-    }
-  
-    80% {
-      clip-path: var(--move2);
-      transform: translate(10px,-10px);
-    }
-  
-    90% {
-      clip-path: var(--move4);
-      transform: translate(-10px,10px);
+      bottom: 5px;
     }
   
     100% {
-      clip-path: var(--move1);
-      transform: translate(0);
+      bottom: 9px;
     }
   }
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,7 +5,7 @@
 
     <NavBar></NavBar>
 
-    <section>
+    <section id="About">
       <h1>Hello</h1>
     </section>
   </main>
